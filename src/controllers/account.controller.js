95db6f00fc811bb0a95d0bedd91360bf4b5c8ca4
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-04-05 20:28:35 +0530

    feat(account): add account domain with auth middleware and modular routing
    - designed and implemented account domain (controller, model, routes)
    - added auth middleware to secure account-related endpoints
    - integrated account routes into main application flow
    - ensured separation of concerns across controllers, services, and middleware
    - improved backend architecture for scalability and maintainability

---
+++ src/controllers/account.controller.js
@@ -0,0 +1,22 @@
+const accountModel = require("../models/account.model")
+const { create } = require("../models/user.model")
+
+
+async function createAccountController(req, res) {
+
+    const user = req.user
+
+    const account = await accountModel.create({
+        user: user._id
+    })
+
+    res.status(201).json({
+        account
+    })
+
+}
+
+
+module.exports = {
+    createAccountController
+}
