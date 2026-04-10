33fe57b372a4c80cec6dc190008d632218741088
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-04-10 14:29:02 +0530

    fix: correct naming issue

--- src/models/blacklist.model.js
+++ src/models/blacklist.model.js
@@ -1,22 +1,21 @@
 const mongoose = require("mongoose");
 
-
 const tokenBlacklistSchema = new mongoose.Schema({
     token: {
         type: String,
         required: [true, "Token is required to blacklist"],
-        unique: [true, "Token is already blacklisted"]
-    }
+        unique: [true, "Token is already blacklisted"],
+    },
 }, {
-    timestamps: true
-})
+    timestamps: true,
+});
 
 
-tokenBlacklistSchema.index({ createdAt: 1 }, {
-    expireAfterSeconds: 60 * 60 * 24 * 3 // 3 days
-})
+tokenBlacklistSchema.index(
+    { createdAt: 1 },
+    { expireAfterSeconds: 60 * 60 * 24 * 3 }
+);
 
-
-const tokenBlacklistModel = mongoose.model("tokenBlacklist", tokenBlacklistSchema)
+const tokenBlacklistModel = mongoose.models.tokenBlacklist || mongoose.model("tokenBlacklist", tokenBlacklistSchema);
 
 module.exports = tokenBlacklistModel;
